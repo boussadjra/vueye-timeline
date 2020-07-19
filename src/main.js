e71525900a5ffdfd86ae906f7895638a3dc52115
Brahim <cb_bousssadjra@esi.dz>
2020-07-19 23:51:55 +0100

    deploy a demo

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueComp from '@vue/composition-api'
 Vue.config.productionTip = false
 
-Vue.use(VueComp)
 
 new Vue({
   render: h => h(App),
